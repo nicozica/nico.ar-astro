afab7ba073a72ef40050dbc538e54e05798761b7
Nicolás Zicarelli <nico@argensonix.com.ar>
2025-09-09 22:03:40 -0300

    Added paginator and other enhancements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   srcDir: 'src',
   integrations: [],
+  output: 'server', // Enable server-side rendering for dynamic pages
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
